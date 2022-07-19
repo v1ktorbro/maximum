75e105d492e9baa79394493e05eeb0cc5c99a47b
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-19 17:57:12 +0500

    feat(Main): connect SectionCards

---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import './Main.scss';
+import React from 'react';
+import SectionCards from '../SectionCards/SectionCards';
+
+export default function Main() {
+  return (
+    <main className='main'>
+      <SectionCards />
+    </main>
+  );
+}
