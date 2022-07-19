01649588ff102a20d0463ede960a586bfbaef861
Viktor Abrosimov <vitya.abrosimoff2016@ya.ru>
2022-07-19 23:52:18 +0500

    refactor(Card): make up the title and vin-code acording to the layout

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,10 +7,13 @@ export default function Card() {
   const initialCard = initialKiaListCars.list[3].feedData;
   return (
       <aside className="card" style={{maxWidth: '20.2rem'}} >
-        <h5 className="card-title">{`${initialCard.brandName} ${initialCard.modelName} ${initialCard.equipmentVariantName}`}</h5>
+        <h5 className="card-title mb-2">
+          {`${initialCard.brandName} ${initialCard.modelName} ${initialCard.equipmentVariantName}`}
+          <span className="badge badge-secondary">{initialCard.modelYear}</span>
+        </h5>
+        <h6 className="card-subtitle mb-3 text-muted">{initialCard.vin}</h6>
         <img className="card-img-top" src={defaultImage} alt="Card image cap"></img>
         <div className="card-body">
-          <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
           <a href="#" className="card-link">Card link</a>
           <a href="#" className="card-link">Another link</a>
