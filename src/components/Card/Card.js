e90539a285d64bef1c789df7e08d0119de4a78ec
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-20 17:09:04 +0500

    feat(Card): create price block

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,8 +4,9 @@ import initialKiaListCars from '../../utils/initialKiaListCars';
 import defaultImage from '../../images/default_image.jpg'
 
 export default function Card() {
-  const initialCard = initialKiaListCars.list[13].feedData;
-  // console.log(initialCard.baseOptions[0].);
+  const initialCard = initialKiaListCars.list[5].feedData;
+  console.log(initialCard);
+
   return (
       <aside className="card" style={{maxWidth: '20.2rem'}} >
         <h5 className="card-title mb-2">
@@ -15,9 +16,7 @@ export default function Card() {
         <h6 className="card-subtitle mb-3 text-muted">{initialCard.vin}</h6>
         <img className="card-img-top" src={defaultImage} alt="Card image cap"></img>
         <div className="card-body">
-
           <ul className='card__list-of-characteristics'>
-
             <li className='card__name-item-list card__name-item-list_engine'>
               <h6 className="card-subtitle mt-1 text-muted">Двигатель</h6>
               <p className='card__value-of-characteristic'>
@@ -49,19 +48,26 @@ export default function Card() {
                 {initialCard.colorByClassifierName.replace(/( |^)[а-яёa-z]/g, initialCard.colorByClassifierName[0].toUpperCase())}
               </p>
             </li>
-
           </ul>
 
-            <div className='card__name-item-list card__name-item-list_options'>
-              <h6 className="card-subtitle mt-0 text-muted">Пакеты</h6>
-              <div className='card__wrapper-block-options'>
-                <p className='card__value-of-characteristic'>
-                  {initialCard.baseOptions[0].optionName}
-                </p>
-                  <button className='card__btn-options-more'>{`(+ ещё ${initialCard.baseOptions.length - 1}) пакета`}</button>
-              </div>
+          <div className='card__name-item-list card__name-item-list_options'>
+            <h6 className="card-subtitle mt-0 text-muted">Пакеты</h6>
+            <div className='card__wrapper-block-options'>
+              <p className='card__value-of-characteristic'>
+                {initialCard.baseOptions[0].optionName}
+              </p>
+                <button className='card__btn-options-more'>{`(+ ещё ${initialCard.baseOptions.length - 1}) пакета`}</button>
             </div>
+          </div>
 
+          <div className='card__price-container'>
+            <span className='card__price-main'>{initialCard.autoPrice}</span>
+            <p className='card__price-additional-options'>
+              Доп. опции на
+              <span>{initialCard.autoPriceRrc}</span>
+            </p>
+            <button type="button" className={`btn ${!initialCard.sale ? "btn-success" :  "btn-secondary"}`}>{!initialCard.sale ? 'Купить' : 'В поставке'}</button>
+          </div>
         </div>
       </aside>
   );
