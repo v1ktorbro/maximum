5c9e35ce9307c7c3c8ab19e42e82e32998ea6219
Viktor Abrosimov <vitya.abrosimoff2016@ya.ru>
2022-07-20 23:22:57 +0500

    feat(arrWithImage): create, export, connect

---
+++ src/utils/arrWithImages.js
@@ -0,0 +1,14 @@
+export const arrWithImages = [
+  {
+    nameImage: 'default_image_1.jpg' 
+  },
+  {
+    nameImage: 'default_image_2.jpeg' 
+  },
+  {
+    nameImage: 'default_image_3.jpg' 
+  },
+  {
+    nameImage: 'default_image_4.jpg' 
+  },
+];
