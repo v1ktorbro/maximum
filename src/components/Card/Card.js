68c84ae1916b4579c061ecb16747897fd0fbf37b
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-20 13:07:05 +0500

    feat(Card): layout list charasteristik of car

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,7 +4,8 @@ import initialKiaListCars from '../../utils/initialKiaListCars';
 import defaultImage from '../../images/default_image.jpg'
 
 export default function Card() {
-  const initialCard = initialKiaListCars.list[3].feedData;
+  const initialCard = initialKiaListCars.list[13].feedData;
+  // console.log(initialCard.baseOptions[0].);
   return (
       <aside className="card" style={{maxWidth: '20.2rem'}} >
         <h5 className="card-title mb-2">
@@ -14,9 +15,53 @@ export default function Card() {
         <h6 className="card-subtitle mb-3 text-muted">{initialCard.vin}</h6>
         <img className="card-img-top" src={defaultImage} alt="Card image cap"></img>
         <div className="card-body">
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-          <a href="#" className="card-link">Card link</a>
-          <a href="#" className="card-link">Another link</a>
+
+          <ul className='card__list-of-characteristics'>
+
+            <li className='card__name-item-list card__name-item-list_engine'>
+              <h6 className="card-subtitle mt-1 text-muted">Двигатель</h6>
+              <p className='card__value-of-characteristic'>
+                {`${initialCard.engine.engineCapacity} л`}
+                <span className='card__symbol-green-slash'>&nbsp;/&nbsp;</span>
+                {`${initialCard.engine.enginePower} лc`} 
+                <span className='card__symbol-green-slash'>&nbsp;/&nbsp;</span>
+                {initialCard.equipmentVariantFuelType.toLowerCase()}
+              </p>
+            </li>
+
+            <li className='card__name-item-list card__name-item-list_auto-probeg'>
+              <h6 className="card-subtitle mt-1 text-muted">Пробег</h6>
+              <p className='card__value-of-characteristic'>
+                {`${initialCard.autoProbeg} км`}
+              </p>
+            </li>
+
+            <li className='card__name-item-list card__name-item-list_transmission'>
+              <h6 className="card-subtitle mt-1 text-muted">Кпп</h6>
+              <p className='card__value-of-characteristic'>
+                {`${initialCard.equipmentVariantTransmissionType}`}
+              </p>
+            </li>
+            
+            <li className='card__name-item-list card__name-item-list_color-car'>
+              <h6 className="card-subtitle mt-1 text-muted">Цвет</h6>
+              <p className='card__value-of-characteristic'>
+                {initialCard.colorByClassifierName.replace(/( |^)[а-яёa-z]/g, initialCard.colorByClassifierName[0].toUpperCase())}
+              </p>
+            </li>
+
+          </ul>
+
+            <div className='card__name-item-list card__name-item-list_options'>
+              <h6 className="card-subtitle mt-0 text-muted">Пакеты</h6>
+              <div className='card__wrapper-block-options'>
+                <p className='card__value-of-characteristic'>
+                  {initialCard.baseOptions[0].optionName}
+                </p>
+                  <button className='card__btn-options-more'>{`(+ ещё ${initialCard.baseOptions.length - 1}) пакета`}</button>
+              </div>
+            </div>
+
         </div>
       </aside>
   );
